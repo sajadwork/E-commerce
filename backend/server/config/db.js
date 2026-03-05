0d94c6434614c6523a39cce3fcb7e18135727b60
sajad <sajadwork@gmail.com>
2026-03-05 19:46:26 +0530

    some changes

--- backend/server/config/db.js
+++ backend/server/config/db.js
@@ -4,7 +4,7 @@ import { env } from './env.js';
 export const connectDB = async () => {
     try {
         const conn = await mongoose.connect(env.MONGO_URI);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
+        console.log("✅ MongoDB connected");
     } catch (error) {
         console.error(`Error: ${error.message}`);
         process.exit(1);
